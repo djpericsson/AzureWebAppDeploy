af594a2308695a6fd47d8c389414f036af00524c
Peter Ericsson <peter.ericsson@addlevel.se>
2017-08-04 11:36:54 +0200

    --

--- App-RegistrationDeployment.ps1
+++ App-RegistrationDeployment.ps1
@@ -52,7 +52,7 @@ $Webclient                       = New-Object System.Net.Webclient
 $Webclient.UseDefaultCredentials = $true
 $Webclient.Proxy.Credentials     = $Webclient.Credentials
 $Webclient.Encoding              = [System.Text.Encoding]::UTF8
-$Webclient.CachePolicy           = New-Object System.Net.Cache.HttpRequestCachePolicy([System.Net.Cache.HttpRequestCacheLevel]::NoCacheNoStore)
+#$Webclient.CachePolicy           = New-Object System.Net.Cache.HttpRequestCachePolicy([System.Net.Cache.HttpRequestCacheLevel]::NoCacheNoStore)
 
 #Start measuring time to complete script
 $Measure = [System.Diagnostics.Stopwatch]::StartNew()
@@ -656,7 +656,7 @@ $restPayload = ConvertTo-Json -InputObject $restPayload -Depth 4
 
 $token = Get-AuthorizationToken -TenantName $tenantName
 
-Write-Output "Lifetime: $($token.Lifetime)"
+Write-Output "ExpiresOn: $($token.ExpiresOn.LocalDateTime)"
 
 $authorizationHeader = @{
     "Content-Type"  = "application/json"
--- Run-Deploy.ps1
+++ Run-Deploy.ps1
@@ -7,7 +7,6 @@ $Webclient                       = New-Object System.Net.Webclient
 $Webclient.UseDefaultCredentials = $true
 $Webclient.Proxy.Credentials     = $Webclient.Credentials
 $Webclient.Encoding              = [System.Text.Encoding]::UTF8
-$Webclient.CachePolicy           = New-Object System.Net.Cache.RequestCachePolicy("BypassCache")
 
 $scriptPath = ($Webclient.DownloadString("$RepoURL/App-RegistrationDeployment.ps1"))
 Invoke-Command -ScriptBlock ([scriptblock]::Create($scriptPath)) -ArgumentList $Location,$Security_Admins,$DynamicsAXApiId,$RepoURL
