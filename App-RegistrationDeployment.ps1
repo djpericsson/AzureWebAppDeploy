3a08088fbb83c22fee7386dbbae06637d9f09037
Peter Ericsson <peter.ericsson@addlevel.se>
2017-08-07 09:31:04 +0200

    --

--- App-RegistrationDeployment.ps1
+++ App-RegistrationDeployment.ps1
@@ -65,20 +65,22 @@ Write-Output "------------------------------------------------------------------
 $DependencyValidation = $True
 
 #Download the Helper-Module
-If ($($StatusCodeHelper = Get-UrlStatusCode -Url "$RepoURL/Helper-Module.ps1") -ne 200) {
+$StatusCodeHelper = Get-UrlStatusCode -Url "$RepoURL/Helper-Module.ps1"
+If ($StatusCodeHelper -ne 200) {
     Write-Warning "Helper-Module location could not be verified."
     Write-Warning "Url: $RepoURL/Helper-Module.ps1"
-    Write-Warning $StatusCodeHelper
+    Write-Warning "UrlStatusCode $StatusCodeHelper"
     $DependencyValidation = $False
 } Else {
     $Webclient.DownloadString("$RepoURL/Helper-Module.ps1") | Invoke-Expression
 }
 
 #Download and convert the configuration data file as Hash Table
-If ($($StatusCodeConfiguration = Get-UrlStatusCode -Url "$RepoURL/ConfigurationData.psd1") -ne 200) {
+$StatusCodeConfiguration = Get-UrlStatusCode -Url "$RepoURL/ConfigurationData.psd1"
+If ($StatusCodeConfiguration -ne 200) {
     Write-Warning "ConfigurationData.psd1 location could not be verified."
     Write-Warning "Url: $RepoURL/ConfigurationData.psd1"
-    Write-Warning $StatusCodeConfiguration
+    Write-Warning "UrlStatusCode $StatusCodeConfiguration"
     $DependencyValidation = $False
 } Else {
     [hashtable]$ConfigurationData = Get-ConfigurationDataAsObject -ConfigurationData ($Webclient.DownloadString("$RepoURL/ConfigurationData.psd1") | Invoke-Expression)
@@ -90,11 +92,11 @@ Write-Host $LogFile
 
 Invoke-Logger -Message "Helper-Module location could not be verified." -Severity W -Category "Helper-Module"
 Invoke-Logger -Message "Url: $RepoURL/Helper-Module.ps1" -Severity W -Category "Helper-Module"
-Invoke-Logger -Message $UrlStatusCode -Severity W -Category "Helper-Module"
+Invoke-Logger -Message "UrlStatusCode $UrlStatusCode" -Severity W -Category "Helper-Module"
 
 Invoke-Logger -Message "ConfigurationData.psd1 location could not be verified." -Severity W -Category "Configuration"
 Invoke-Logger -Message "Url: $RepoURL/ConfigurationData.psd1" -Severity W -Category "Configuration"
-Invoke-Logger -Message $UrlStatusCode -Severity W -Category "Configuration"
+Invoke-Logger -Message "UrlStatusCode $UrlStatusCode" -Severity W -Category "Configuration"
 
 break
 
